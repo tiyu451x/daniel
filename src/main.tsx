7c7714bf89a8672bafd9eb4de4a0c78d4670d515
Chiro <daniel.ichiro.p.t.s@gmail.com>
2026-08-14 12:01:41 +0700

    testing 5

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-// import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import Phaser from 'phaser';
 import { StartGame } from './game/Game';
